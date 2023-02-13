d0f4a6fd5a5e3ead5e358416a62857417750e1b2
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-02-13 11:43:15 -0700

    Add "path" loading  - The paths components are the thing that takes longer - we should    implement something to load them progressively  as opposed to all    at once.    This will load all of them without blocking the rendering

---
+++ src/hooks/useBackgroundTask.ts
@@ -0,0 +1,26 @@
+import {DependencyList, useEffect, useState} from "react";
+
+export type BackgroundTaskState<T> = {
+    loading: true
+} | {
+    loading: false;
+    value: T
+};
+
+export const useBackgroundTask = <T>(callback: () => T, deps?: DependencyList): BackgroundTaskState<T> => {
+    const [state, setState] = useState<BackgroundTaskState<T>>({
+        loading: true
+    });
+
+    useEffect(() => {
+        (async () => {
+            const value = callback();
+            setState({
+                loading: false,
+                value
+            });
+        })();
+    }, deps);
+
+    return state;
+}
