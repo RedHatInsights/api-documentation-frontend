90ab8c72a25783e083bda2f1b059f775ca0ce13b
Eric Winchell <ewinchel@redhat.com>
2024-11-22 10:08:03 -0500

    fix toggle button positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
           <PageSection variant={PageSectionVariants.light} className="pf-v5-u-p-md">
             <Flex direction={{ default: 'rowReverse' }}>
               <Split className="apid-split-l-pagination pf-v5-u-align-items-center-on-xl">
-                <SplitItem className="pf-v5-u-pb-md pf-v5-u-pt-md-on-md pf-v5-u-pl-sm-on-md">
+                <SplitItem className="pf-v5-u-pb-md pf-v5-u-pt-md-on-md pf-v5-u-pl-sm-on-md" isFilled>
                   <Button
                     className="pf-v5-u-mr-sm pf-v5-u-mb-sm"
                     component="a"
